34b6ce437a91ef6a65922718db87aa2002878581
StyrO <karam.bd.kb@gmail.com>
2024-11-29 17:41:22 +0100

    Update teams color name

--- src/data/teams.js
+++ src/data/teams.js
@@ -1,11 +1,11 @@
 const teams = [
-  { id: 1, name: "Team Alpha" },
-  { id: 2, name: "Team Beta" },
-  { id: 3, name: "Team Gamma" },
-  { id: 4, name: "Team Delta" },
-  { id: 5, name: "Team Epsilon" },
-  { id: 6, name: "Team Zeta" },
-  { id: 7, name: "Team Eta" }
+  { id: 1, name: "Team Black" },
+  { id: 2, name: "Team Red" },
+  { id: 3, name: "Team Gray" },
+  { id: 4, name: "Team Orange" },
+  { id: 5, name: "Team White" },
+  { id: 6, name: "Team Beige" },
+  { id: 7, name: "Team Blue" }
 ];
 
 export default teams; 
